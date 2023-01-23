6d9d701d593cac57d478f16fb8066089825b6139
Mariyan Maksimov <mariyan.pickup@gmail.com>
2023-01-23 16:53:15 +0200

    added project cards, new pic and couple of structure changes

--- src/app/core/components/projects/projects.component.ts
+++ src/app/core/components/projects/projects.component.ts
@@ -3,6 +3,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
     selector: 'app-projects',
@@ -10,6 +12,14 @@ import { MatDividerModule } from '@angular/material/divider';
     styleUrls: ['./projects.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
-    imports: [MatCardModule, MatProgressBarModule, MatDividerModule],
+    imports: [
+        MatCardModule,
+        MatProgressBarModule,
+        MatDividerModule,
+        MatButtonModule,
+        MatGridListModule,
+    ],
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+    longText = `Javascript, Typescript, Angular`;
+}

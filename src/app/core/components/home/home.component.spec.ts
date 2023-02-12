7e6f348524c4cfeab85a916cef91b8d610a55bd2
Mariyan Maksimov <mariyan.pickup@gmail.com>
2023-02-12 19:26:25 +0200

    fixed tests

--- src/app/core/components/home/home.component.spec.ts
+++ src/app/core/components/home/home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +9,7 @@ describe('HomeComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [HomeComponent],
+            imports: [HomeComponent, AppRoutingModule],
         }).compileComponents();
 
         fixture = TestBed.createComponent(HomeComponent);
